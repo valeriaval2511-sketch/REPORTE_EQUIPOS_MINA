f9441e15e77e1c6f1d0dcc8c65ddf2c80a6812a6
valeriaval2511-sketch <valeriaval2511@gmail.com>
2026-04-14 13:12:08 -0500

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,26 @@
-self.addEventListener("install", event => {
+const CACHE = "app-mina-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
